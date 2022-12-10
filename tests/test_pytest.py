35d73b0d96f86129b580511910220c8c94b678e2
EdGoh95 <33577616+EdGoh95@users.noreply.github.com>
2022-12-10 15:10:12 +0800

    Added test_pytest.py for testing

---
+++ tests/test_pytest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Validating Your Code, Data And Models: Rethinking Unit Testing For Data Quality
+"""
+import pandas as pd
+
+def test_upper():
+    assert 'foo'.upper() == 'FOO'
+
+df = pd.DataFrame(data = {'Data': [1, 2, 3]})
+def test_min():
+    assert df.min().values[0] > 0
+def test_max():
+    assert df.max().values[0] < 100
