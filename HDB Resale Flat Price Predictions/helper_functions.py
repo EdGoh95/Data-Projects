408fa7f59f92fbbad1808610b3b86df80685713c
EdGoh95 <33577616+EdGoh95@users.noreply.github.com>
2023-08-26 23:45:47 +0800

    Modified and shifted helper_functions.py into HDB Resale Flat Price Predictions folder

--- HDB Resale Flat Price Predictions/helper_functions.py
+++ HDB Resale Flat Price Predictions/helper_functions.py
@@ -11,14 +11,14 @@ def reverse_lookup(df, column, lookup_dict, new_column):
     return df
 
 def geocoding_Nominatim(unique_address):
-    locator = Nominatim(user_agent = 'edwin.goh@ncs.com.sg', timeout = 100000)
+    locator = Nominatim(user_agent = <YOUR_ACCOUNT_ID>, timeout = 100000)
     geocode = RateLimiter(locator.geocode, max_retries = 5)
     print('Converting {} to GPS coordinates'.format(unique_address))
     location = geocode(unique_address, country_codes = 'SG')
     return location.latitude, location.longitude
    
 def geocoding_GoogleMaps(unique_address):
-    locator = GoogleV3(api_key = 'AIzaSyAVcsEuf2KiG512u15E4fVNdsMZKWJAtuk', timeout = 100000)
+    locator = GoogleV3(api_key = <YOUR_GOOGLEV3_API_KEY>, timeout = 100000)
     geocode = RateLimiter(locator.geocode, min_delay_seconds = 0.01, max_retries = 5)
     print('Converting {} to GPS coordinates'.format(unique_address))
     location = geocode(unique_address, region = 'SG', bounds = [[1.203748, 103.585763], [1.481474, 104.041009]])
