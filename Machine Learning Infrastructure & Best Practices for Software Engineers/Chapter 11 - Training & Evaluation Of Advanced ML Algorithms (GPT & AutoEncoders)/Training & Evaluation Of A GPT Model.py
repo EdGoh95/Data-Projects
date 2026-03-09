5296bc8aabb645a5e177072c3f5843ea97a010bb
EdGoh95 <edwin.spartan117@yahoo.com.sg>
2026-03-10 00:47:23 +0800

    Upload "Chapter 11 - Training & Evaluation of Advanced ML Algorithms (GPT & AutoEncoders)" folder (containing large files)

---
+++ Machine Learning Infrastructure & Best Practices for Software Engineers/Chapter 11 - Training & Evaluation Of Advanced ML Algorithms (GPT & AutoEncoders)/Training & Evaluation Of A GPT Model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55a419fb186ae2c4adaf07831466bc02f48e70e262fadada395dc3aa38bc7592
+size 3213
---
+++ Machine Learning Infrastructure & Best Practices for Software Engineers/Chapter 11 - Training & Evaluation Of Advanced ML Algorithms (GPT & AutoEncoders)/Training & Evaluation Of An AutoEncoder.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ff3e4e474ad28cb4b22b7913efef9e563a48521c14e139423ee4477f25f908b
+size 3006
